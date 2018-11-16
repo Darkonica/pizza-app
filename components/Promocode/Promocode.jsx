5d4ea0a081df1194cbb5802cb2d5280697fff2ff
Vladimir Novichikhin <darkonica@gmail.com>
2018-11-16 01:34:07 -0800

    promo fix

--- components/Promocode/Promocode.jsx
+++ components/Promocode/Promocode.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class Promocode extends Component {
+
+    state = {
+        value: ''
+    };
+
+    onChange = (e) => {
+        const value = e.target.value;
+        console.log(value);
+        this.setState({
+            ...this.state,
+            value
+        });
+    }
+
+    onSubmit = () => {
+
+    }
+
     render() {
+        const { value } = this.state;
+
         return (
             <div>
-                <input type="text" placeholder="Введите промокод" />
+                <input
+                    type="text"
+                    placeholder="Введите промокод"
+                    value={value}
+                    onChange={this.onChange}
+                />
                 <button>Применить</button>
             </div>
         );
