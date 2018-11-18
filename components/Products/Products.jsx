4618221e682537f23958030eb713c7cfcfef5d8a
Vladimir Novichikhin <darkonica@gmail.com>
2018-11-18 09:51:56 -0800

    Product component pizza part done

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 
 const StyledProducts = styled.div`
     display: flex;
-    justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 0 -20px;
 `;
 
 class Products extends Component {
@@ -15,6 +16,13 @@ class Products extends Component {
                     <Product
                         
                     />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    
                 </StyledProducts>
             </div>
         );
