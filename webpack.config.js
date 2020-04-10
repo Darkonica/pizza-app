b305ca48c59010af909b1582baa957e1cfd95acb
Vladimir Novichikhin <darkonica@gmail.com>
2020-04-10 21:42:18 +0300

    packages updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const postcssNormalize = require("postcss-normalize");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const postcssNormalize = require('postcss-normalize');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.svg$/,
-        use: ["@svgr/webpack"],
+        use: ['@svgr/webpack'],
       },
       {
         test: /\.(s?)css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
       {
         test: /\.(png|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   optimization: {
     minimizer: [new UglifyJsPlugin()],
@@ -59,16 +59,16 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new CopyWebpackPlugin([
       {
-        from: "src/static/images/",
+        from: 'src/static/images/',
       },
     ]),
     new ImageminWebpackPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
-      disable: process.env.NODE_ENV !== "production", // Disable during development
+      disable: process.env.NODE_ENV !== 'production', // Disable during development
     }),
   ],
 };
